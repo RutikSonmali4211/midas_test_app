5b492710915fe2dae27cc0308f7dc6ce836a8325
RutikSonmali4211 <rutik.s@ergobite.com>
2023-10-29 23:32:32 +0530

    updated the flutter main file

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 
 UserController userController = Get.put(UserController());
-void main() async {
+Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
   // WidgetsBinding.instance.addObserver(AppLifecycleObserver());
