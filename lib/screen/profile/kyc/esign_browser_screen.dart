06c0a1c0edf94cd5c41fa61f6bb4f9bc3983fd66
RutikSonmali4211 <rutik.s@ergobite.com>
2024-01-02 11:34:07 +0530

    updated code for dependency updatee webview

--- lib/screen/profile/kyc/esign_browser_screen.dart
+++ lib/screen/profile/kyc/esign_browser_screen.dart
@@ -1,13 +1,14 @@
 // ignore_for_file: use_build_context_synchronously
 
 import 'package:flutter/material.dart';
+import 'package:flutter_inappwebview/flutter_inappwebview.dart';
 import 'package:get/get.dart';
+import 'package:http_interceptor/extensions/extensions.dart';
 import 'package:midas/controller/kyc/kyc_controller.dart';
 import 'package:midas/constant/size_util.dart';
 import 'package:midas/screen/profile/kyc/esign_required_screen.dart';
 import 'package:midas/screen/profile/kyc/kyc_submitted.dart';
 import 'package:midas/widgets/alert_message/alert_message.dart';
-import 'package:webview_flutter/webview_flutter.dart';
 
 class EsignBrowserScreen extends StatefulWidget {
   final String postbackurl;
@@ -18,7 +19,7 @@ class EsignBrowserScreen extends StatefulWidget {
 }
 
 class _EsignBrowserScreenState extends State<EsignBrowserScreen> {
-  late WebViewController webViewController;
+  late InAppWebViewController webViewController;
 
   KYCController kycController = Get.put(KYCController());
 
@@ -34,45 +35,97 @@ class _EsignBrowserScreenState extends State<EsignBrowserScreen> {
         children: [
           SizedBox(height: SizeUtil.verticalSpacingMedium(context)),
           Expanded(
-            child: WebView(
-              gestureNavigationEnabled: true,
-              initialUrl: widget.postbackurl,
-              javascriptMode: JavascriptMode.unrestricted,
-              onWebViewCreated: (controller) {
-                webViewController = controller;
-              },
-              navigationDelegate: (NavigationRequest request) async {
-                if (request.url.contains(
-                    'http://devmidasadmin.ergobite.com:3000/webhook')) {
-                  final Uri uri = Uri.parse(request.url);
-                  final Map<String, String> parameters = uri.queryParameters;
-                  if (parameters.containsKey('status')) {
-                    String statusValue = parameters['status']!;
-                    if (statusValue == 'successful') {
-                      bool isSuccess = await kycController
-                          .updateKycStatusAfterEsignCompleted(
-                              "submitted", context);
-                      if (isSuccess) {
-                        Navigator.of(context).pushReplacement(
-                            MaterialPageRoute(
-                                builder: (context) =>
-                                    const KycSubmittedPhase()));
-                      }
-                    } else {
-                      showErrorAlert(
-                          "your Esign is not completed please try again");
-                      Navigator.of(context).maybePop(MaterialPageRoute(
-                          builder: (context) => const EsignRequired()));
-                    }
-                  }
-                  return NavigationDecision.prevent;
-                }
-                return NavigationDecision.navigate;
-              },
+            child: InAppWebView(
+              initialUrlRequest: URLRequest(
+                url: WebUri.uri(widget.postbackurl.toUri()),
+              ),
+              onWebViewCreated: (controller) =>
+                  webViewController = webViewController,
+              onLoadStart: onPageChange,
+              // onUpdateVisitedHistory: onPageChange,
             ),
           ),
         ],
       ),
     );
   }
+
+  void onPageChange(
+    InAppWebViewController controller,
+    Uri? uri,
+  ) async {
+    String url = uri.toString();
+    if (url.contains('http://devmidasadmin.ergobite.com:3000/webhook')) {
+      final Uri uri = Uri.parse(url);
+      final Map<String, String> parameters = uri.queryParameters;
+      if (parameters.containsKey('status')) {
+        String statusValue = parameters['status']!;
+        if (statusValue == 'successful') {
+          bool isSuccess = await kycController
+              .updateKycStatusAfterEsignCompleted("submitted", context);
+          if (isSuccess) {
+            Navigator.of(context).pushReplacement(MaterialPageRoute(
+                builder: (context) => const KycSubmittedPhase()));
+          }
+        } else {
+          showErrorAlert("your Esign is not completed please try again");
+          Navigator.of(context).maybePop(
+              MaterialPageRoute(builder: (context) => const EsignRequired()));
+        }
+      }
+    }
+  }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// WebView(
+//               gestureNavigationEnabled: true,
+//               initialUrl: widget.postbackurl,
+//               javascriptMode: JavascriptMode.unrestricted,
+//               onWebViewCreated: (controller) {
+//                 webViewController = controller;
+//               },
+//               navigationDelegate: (NavigationRequest request) async {
+//                 if (request.url.contains(
+//                     'http://devmidasadmin.ergobite.com:3000/webhook')) {
+//                   final Uri uri = Uri.parse(request.url);
+//                   final Map<String, String> parameters = uri.queryParameters;
+//                   if (parameters.containsKey('status')) {
+//                     String statusValue = parameters['status']!;
+//                     if (statusValue == 'successful') {
+//                       bool isSuccess = await kycController
+//                           .updateKycStatusAfterEsignCompleted(
+//                               "submitted", context);
+//                       if (isSuccess) {
+//                         Navigator.of(context).pushReplacement(
+//                             MaterialPageRoute(
+//                                 builder: (context) =>
+//                                     const KycSubmittedPhase()));
+//                       }
+//                     } else {
+//                       showErrorAlert(
+//                           "your Esign is not completed please try again");
+//                       Navigator.of(context).maybePop(MaterialPageRoute(
+//                           builder: (context) => const EsignRequired()));
+//                     }
+//                   }
+//                   return NavigationDecision.prevent;
+//                 }
+//                 return NavigationDecision.navigate;
+//               },
+//             ),
